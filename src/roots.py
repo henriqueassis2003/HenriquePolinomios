471615eebab3c5affbaee44cea6ed2dc277371a8
henriqueassis2003 <128740531+henriqueassis2003@users.noreply.github.com>
2023-06-06 12:13:05 -0300

    Update roots.py

--- src/roots.py
+++ src/roots.py
@@ -3,6 +3,10 @@ def ext_evaluate(polynom, x):
   for degree in polynom:
       sum_of_x += polynom[degree] * (x ** degree)
   return sum_of_x
+
+def symDerivate(polinomial_dict):
+    return {(i - 1) : (polinomial_dict[i] * i) for i in polinomial_dict}
+
 def ext_sucessive_appx(polinomial_dict, min_interval, max_interval):
         diference = 1
         if self.evaluate(polinomial_dict, min_interval) <= evaluate(polinomial_dict, max_interval):
