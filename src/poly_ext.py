0801f660039ef6d5c5fa5a18a9c0cb957e31df88
henriqueassis2003 <128740531+henriqueassis2003@users.noreply.github.com>
2023-06-06 11:36:03 -0300

    Update poly_ext.py

--- src/poly_ext.py
+++ src/poly_ext.py
@@ -3,4 +3,32 @@ def ext_evaluate(polynom, x):
   for degree in polynom:
       sum_of_x += polynom[degree] * (x ** degree)
   return sum_of_x
-      
+def ext_sucessive_appx(polinomial_dict, min_interval, max_interval):
+        diference = 1
+        if self.evaluate(polinomial_dict, min_interval) <= evaluate(polinomial_dict, max_interval):
+            if evaluate(polinomial_dict, min_interval) == 0:
+                return min_interval
+            
+            while diference >= 1e-15:
+                if evaluate(polinomial_dict, (min_interval+max_interval)/2) > 0:
+                    max_interval=(min_interval+max_interval)/2
+                if evaluate(polinomial_dict, (min_interval + max_interval) / 2) < 0:
+                    min_interval = (min_interval + max_interval) / 2
+                diference = abs(evaluate(polinomial_dict, (min_interval + max_interval) / 2))
+                if evaluate(polinomial_dict, (min_interval + max_interval) / 2) == 0:
+                    return  (min_interval + max_interval) / 2
+            return  (min_interval + max_interval) / 2
+        if evaluate(polinomial_dict, min_interval) > evaluate(polinomial_dict, max_interval):
+            if evaluate(polinomial_dict, max_interval) == 0:
+                return min_interval
+              
+            while diference >= 1e-15:
+                if evaluate(polinomial_dict, (min_interval+max_interval)/2) > 0:
+                    min_interval=(min_interval+max_interval)/2
+                if evaluate(polinomial_dict, (min_interval + max_interval) / 2) < 0:
+                    max_interval = (min_interval + max_interval) / 2
+                diference=abs(evaluate(polinomial_dict, (min_interval + max_interval) / 2))
+                if evaluate(polinomial_dict, (min_interval + max_interval) / 2) == 0:
+                    return  (min_interval + max_interval) / 2
+            return (min_interval + max_interval) / 2
+
