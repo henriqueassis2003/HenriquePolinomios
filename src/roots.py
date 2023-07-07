997ed825e625a1887361f3dad6426297338af73d
henriqueassis2003 <128740531+henriqueassis2003@users.noreply.github.com>
2023-07-07 01:14:11 -0300

    Update roots.py

--- src/roots.py
+++ src/roots.py
@@ -1,52 +1,26 @@
-def ext_evaluate(polynom, x):
-  sum_of_x = 0
-  for degree in polynom:
-      sum_of_x += polynom[degree] * (x ** degree)
-  return sum_of_x
+def ext_evaluate(polynomial, x):
+    sum_of_x = 0
+    for degree in polynomial:
+        sum_of_x += polynomial[degree] * (x ** degree)
+    return sum_of_x
 
-def symDerivate(polinomial_dict):
-    return {(i - 1) : (polinomial_dict[i] * i) for i in polinomial_dict}
+def ext_symDerivate(polynomial_dict):
+    return {(i - 1): (polynomial_dict[i] * i) for i in polynomial_dict}
 
-def ext_sucessive_appx(polinomial_dict, min_interval, max_interval):
-        diference = 1
-        if self.evaluate(polinomial_dict, min_interval) <= evaluate(polinomial_dict, max_interval):
-            if evaluate(polinomial_dict, min_interval) == 0:
-                return min_interval
-            
-            while diference >= 1e-15:
-                if evaluate(polinomial_dict, (min_interval+max_interval)/2) > 0:
-                    max_interval=(min_interval+max_interval)/2
-                if evaluate(polinomial_dict, (min_interval + max_interval) / 2) < 0:
-                    min_interval = (min_interval + max_interval) / 2
-                diference = abs(evaluate(polinomial_dict, (min_interval + max_interval) / 2))
-                if evaluate(polinomial_dict, (min_interval + max_interval) / 2) == 0:
-                    return  (min_interval + max_interval) / 2
-            return  (min_interval + max_interval) / 2
-        if evaluate(polinomial_dict, min_interval) > evaluate(polinomial_dict, max_interval):
-            if evaluate(polinomial_dict, max_interval) == 0:
-                return min_interval
-              
-            while diference >= 1e-15:
-                if evaluate(polinomial_dict, (min_interval+max_interval)/2) > 0:
-                    min_interval=(min_interval+max_interval)/2
-                if evaluate(polinomial_dict, (min_interval + max_interval) / 2) < 0:
-                    max_interval = (min_interval + max_interval) / 2
-                diference=abs(evaluate(polinomial_dict, (min_interval + max_interval) / 2))
-                if evaluate(polinomial_dict, (min_interval + max_interval) / 2) == 0:
-                    return  (min_interval + max_interval) / 2
-            return (min_interval + max_interval) / 2
+def ext_successiveAppx(self, min_interval, max_interval,tolerance=1e-12) :
+        itter=0
+        while abs(ext_evaluate(self.polynomial_dict, (min_interval + max_interval) / 2)) >= tolerance:
+            if ext_evaluate(self.polynomial_dict, min_interval) * ext_evaluate(self.polynomial_dict,(min_interval + max_interval) / 2) < 0:
+                max_interval = (min_interval + max_interval) / 2
+            else:
+                min_interval = (min_interval + max_interval) / 2
+            if itter>120:
+               return False
+            itter+=1
+        return (min_interval + max_interval) / 2
 
-    def newtonRaphson(polinomial_dict, min_interval, max_interval):
-        if abs(max_interval - min_interval) < 2:
-            xn = (max_interval + min_interval) / 2
-            for i in range(1, 20):
-                xn -= (evaluate(polinomial_dict, xn)) / (evaluate(symDerivate(), xn))
-            return xn
-        else:
-            for j in range(abs(int(max_interval - min_interval)) // 2 + 1):
-                xn = min_interval + j * (abs(max_interval - min_interval) // 2 + 1)
-                for i in range(1, 100):
-                    xn -= (evaluate(polinomial_dict, xn)) / (evaluate(symDerivate(), xn))
-                if evaluate(polinomial_dict, xn) < 1e-5:
-                    return xn
-        return False
+def ext_newtonRaphson(polynomial_dict, root,cycles=100):
+    derivate_dict=ext_symDerivate(polynomial_dict)
+    for i in range(cycles):
+        root-=ext_evaluate(polynomial_dict,root)/ext_evaluate(derivate_dict,root)
+    return root
